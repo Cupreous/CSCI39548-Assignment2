f4b388015df906e4e174142578a818ee6944c273
Michael Russo <michaelrusso242@aol.com>
2023-10-10 22:39:26 -0400

    Fix Logic For Removing Last Row

--- script.js
+++ script.js
@@ -75,6 +75,11 @@ function removeC() {
             grid.rows[x].deleteCell(-1);
         }
         numCols--;
+        if (numCols == 0) //If nothing left in table, resets numRowss
+        {
+            numRows = 0;
+        }
+        
     } else
     {
         alert("There are no columns left to remove"); 
